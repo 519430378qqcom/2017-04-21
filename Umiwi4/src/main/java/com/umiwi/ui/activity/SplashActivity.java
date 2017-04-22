a8a8f91893eb36bb0a7b63bc6d59f1671441980d
guoyaoshen <amysgyc@163.com>
2017-04-22 14:38:46 +0800

    导航栏 不均分  试读 布局分割线，已购专栏  图标

--- Umiwi4/src/main/java/com/umiwi/ui/activity/SplashActivity.java
+++ Umiwi4/src/main/java/com/umiwi/ui/activity/SplashActivity.java
@@ -8,7 +8,6 @@ import android.os.Environment;
 import android.os.Parcelable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
