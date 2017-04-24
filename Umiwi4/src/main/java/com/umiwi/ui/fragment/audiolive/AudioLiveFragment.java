253c0ee681ca009eee76138e183316bde3f9678f
guoyaoshen <amysgyc@163.com>
2017-04-24 20:45:08 +0800

    直播

--- Umiwi4/src/main/java/com/umiwi/ui/fragment/audiolive/AudioLiveFragment.java
+++ Umiwi4/src/main/java/com/umiwi/ui/fragment/audiolive/AudioLiveFragment.java
@@ -1,5 +1,12 @@
 package com.umiwi.ui.fragment.audiolive;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.umiwi.ui.R;
 import com.umiwi.ui.main.BaseConstantFragment;
 
 /**
@@ -8,4 +15,12 @@ import com.umiwi.ui.main.BaseConstantFragment;
 
 public class AudioLiveFragment extends BaseConstantFragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_audio_live,null);
+
+
+        return view;
+    }
 }
